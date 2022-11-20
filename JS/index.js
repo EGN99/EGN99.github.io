eede19789f08a2a8cf09aa4f3c0684c57ad69822
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2022-11-21 02:53:03 +0300

    added js

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+var btn= document.querySelector(".contactUs");
+
+btn.addEventListener("click",function() {
+    btn.innerHTML = 'You have clicked on the Contact Us button';
+});
+
+var button= document.querySelector(".getInTouch");
+
+button.addEventListener("click",function() {
+    button.innerHTML = `You have clicked on the Get In Touch button`;
+});
+
